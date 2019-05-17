30e3d275ec49c57a7edb05e19c88aba89c832f69
dyzmo1111 <dionizy6@gmail.com>
2019-05-17 20:59:38 +0200

    Mój pomysł na to zadanie

--- src/main/java/pl/javastart/Runner.java
+++ src/main/java/pl/javastart/Runner.java
@@ -1,7 +1,41 @@
 package pl.javastart;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
 class Runner {
-    public static void main(String[] args) {
-        //TODO
-    }
+	public static void main(String[] args) {
+
+		System.out.println("Nazwa klasy: ");
+		Scanner scanner = new Scanner(System.in);
+		String className = scanner.next();
+		invokeMethod(className, "show");
+	}
+
+	public static void invokeMethod(String className, String methodName) {
+
+		String packageName = Runner.class.getPackage().getName();
+		String path = packageName + "." + className;
+
+		try {
+			Class<?> cl = Class.forName(path);
+			Constructor<?> cons = cl.getDeclaredConstructors()[0];
+			cons.setAccessible(true);
+			Object obj = cons.newInstance("JavaStart");
+			Method m = obj.getClass().getDeclaredMethod(methodName);
+			m.invoke(obj);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
